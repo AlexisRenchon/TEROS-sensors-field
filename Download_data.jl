d3c9b28f6d33e2f845a1d9a8c2fc16c937f8ef23
Dr. Alexandre Alexis Renchon <a.renchon@gmail.com>
2019-11-21 15:45:24 -0600

    Update Download_data.jl

--- Download_data.jl
+++ Download_data.jl
@@ -2,10 +2,12 @@
 # Note: working directory must be set to TEROS-sensors-field
 # Check with the command pwd()
 
-url = "http://i3.ytimg.com/vi/J---aiyznGQ/mqdefault.jpg" # test for now
-download(url,"Input\\Data.jpg") # test for now
+url = "https://zentracloud.com/api/v1/readings?sn=z6-04618&end_mrid=10"
+download(url,"Input\\Data.csv")
 
 # I will change this for ZENTRA cloud data.
 # Ideally, I want 1 csv file per ZL6 loggers, or 1 csv file with all ZL6 loggers.
 # I also want this script to NOT redownload existing data (or redownload but then erase old data).
 # The idea is, each time the script runs, it update the Input with complete dataset.
+
+# Need a token
