48dde61188d34c34c5c8dc522206c165f722ea47
Dr. Alexandre Alexis Renchon <a.renchon@gmail.com>
2019-11-26 10:42:54 -0600

    Update Plot_input.jl

--- Plot_input.jl
+++ Plot_input.jl
@@ -1,44 +1,20 @@
-# Plot inputs
+# Plot soil moisture (color) on coordinate (x,y), Alexis 11/25/2019
+
+# This will get all input File Name
+Input_FN = readdir("Input\\")
+n = length(Input_FN) # this is the number of input files, useful later
+
+# Initialize array of dataframes
+using DataFrames
+data = DataFrame[]
 
 # Load data
 using CSV
-Input1 = CSV.read("Input\\1(z6-04628).csv", header=1,normalizenames=true,dateformat="yyyy-mm-dd HH:MM:SS")
-# Input2 = CSV.read("Input\\1(z6-04628)-Configuration 2-1574695017.61.csv", header=1,normalizenames=true,dateformat="yyyy-mm-dd HH:MM:SS")
-Input3 = CSV.read("Input\\3(z6-04619).csv", header=1,normalizenames=true,dateformat="yyyy-mm-dd HH:MM:SS")
-Input4 = CSV.read("Input\\4(z6-04629).csv", header=1,normalizenames=true,dateformat="yyyy-mm-dd HH:MM:SS")
-Input5 = CSV.read("Input\\5(z6-04641).csv", header=1,normalizenames=true,dateformat="yyyy-mm-dd HH:MM:SS")
-Input6 = CSV.read("Input\\6(z6-04638).csv", header=1,normalizenames=true,dateformat="yyyy-mm-dd HH:MM:SS")
-Input7 = CSV.read("Input\\7(z6-04624).csv", header=1,normalizenames=true,dateformat="yyyy-mm-dd HH:MM:SS")
-Input8 = CSV.read("Input\\8(z6-04627).csv", header=1,normalizenames=true,dateformat="yyyy-mm-dd HH:MM:SS")
-Input9 = CSV.read("Input\\9(z6-04620).csv", header=1,normalizenames=true,dateformat="yyyy-mm-dd HH:MM:SS")
-Input10 = CSV.read("Input\\10(z6-04621).csv", header=1,normalizenames=true,dateformat="yyyy-mm-dd HH:MM:SS")
-Input11 = CSV.read("Input\\11(z6-04618).csv", header=1,normalizenames=true,dateformat="yyyy-mm-dd HH:MM:SS")
-
-# rename columns
 col_name = [:DateTime,:SWC_1,:Ts_1,:SWC_2,:Ts_2,:SWC_3,:Ts_3,:SWC_4,:Ts_4,:SWC_5,:Ts_5,:SWC_6,:Ts_6,:Battery_P,:Battery_V,:Pressure,:Log_T]
-old_name1 = names(Input1)
-#old_name2 = names(Input2)
-old_name3 = names(Input3)
-old_name4 = names(Input4)
-old_name5 = names(Input5)
-old_name6 = names(Input6)
-old_name7 = names(Input7)
-old_name8 = names(Input8)
-old_name9 = names(Input9)
-old_name10 = names(Input10)
-old_name11 = names(Input11)
-using DataFrames
-rename!(Input1, f => t for (f, t) = zip(old_name1, col_name))
-# rename!(Input2, f => t for (f, t) = zip(old_name2, col_name))
-rename!(Input3, f => t for (f, t) = zip(old_name3, col_name))
-rename!(Input4, f => t for (f, t) = zip(old_name4, col_name))
-rename!(Input5, f => t for (f, t) = zip(old_name5, col_name))
-rename!(Input6, f => t for (f, t) = zip(old_name6, col_name))
-rename!(Input7, f => t for (f, t) = zip(old_name7, col_name))
-rename!(Input8, f => t for (f, t) = zip(old_name8, col_name))
-rename!(Input9, f => t for (f, t) = zip(old_name9, col_name))
-rename!(Input10, f => t for (f, t) = zip(old_name10, col_name))
-rename!(Input11, f => t for (f, t) = zip(old_name11, col_name))
+for i = 1:n
+    df = CSV.read(string("Input\\",Input_FN[i]),header=col_name,datarow=2,dateformat="yyyy-mm-dd HH:MM:SS")
+    push!(data, df) # push "Insert one or more items at the end of collection"
+end
 
 # Plots space coordinate and SWC by color
 using Plots
