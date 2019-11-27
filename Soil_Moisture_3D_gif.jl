7518e380a7ddd7d818fdd9364475cc06056aa1b9
Dr. Alexandre Alexis Renchon <a.renchon@gmail.com>
2019-11-27 15:45:47 -0600

    Update Soil_Moisture_3D_gif.jl

--- Soil_Moisture_3D_gif.jl
+++ Soil_Moisture_3D_gif.jl
@@ -12,64 +12,68 @@ for i = 1:n
     df = CSV.read(string("Input\\TEROS\\",Input_FN[i]),header=col_name,datarow=2,dateformat="yyyy-mm-dd HH:MM:SS")
     push!(data, df) # push "Insert one or more items at the end of collection"
 end
+
+# Create a continuous half-hourly DateTime vector
+using Dates
+Dtime = collect(Dates.DateTime(DateTime(2019,11,19,00,00,00)):Dates.Minute(30):Dates.DateTime(DateTime(2019,11,28,00,00,00)))
+m = length(Dtime)
+# Initialize SWC matrice with 66 columns, m rows
+SWC = Array{Union{Float64,Missing}}(missing,m,66)
+nextit = collect(0:5:5*n-1)
+for j = 1:11
+    for i = 1:m
+        k = nextit[j]
+        t = findfirst(x -> x == Dtime[i],data[j].DateTime)
+        if isnothing(t) == false
+            SWC[i,j+k] = data[j].SWC_1[t]
+            SWC[i,j+1+k] = data[j].SWC_2[t]
+            SWC[i,j+2+k] = data[j].SWC_3[t]
+            SWC[i,j+3+k] = data[j].SWC_4[t]
+            SWC[i,j+4+k] = data[j].SWC_5[t]
+            SWC[i,j+5+k] = data[j].SWC_6[t]
+        end
+    end
+end
+# could find something more elegent to do the loop below...
+for j = 1:66
+    for i = 1:m
+        if SWC[i,j] < 0.35 || SWC[i,j] > 0.5
+            SWC[i,j] = NaN
+        end
+    end
+end
+
 MD = CSV.read("Input\\Metadata.csv")
 x = MD.x*12.5
 y = MD.y*12.5
 # altitude = rand(228:236,66)
-using Dates
-# Differs from here, calculate z for different dates
-# Initialize array of Dates
-D1 = Array{DateTime}(undef, 6)
-#i = collect(1:6)
-i = 1
-for d = [23,24,25]
-    for h = [0,8]
-        D1[i] = DateTime(2019,11,d,h,00,00)
-        global i += 1 # i = i + 1
-    end
-end
-# Create 6 vector z
-z = Array{Union{Float64,Missing}}(missing, 66, 6)
-# z2 = Array{Array}(undef, 6)
-# z2[1] = [1,2,3]
-nextit = collect(0:5:5*n-1)
-for dt = 1:6
-    for i = 1:n
-        j = nextit[i]
-        t = findall(x -> x == D1[dt],data[i].DateTime)
-        z[i+j,dt] = data[i].SWC_1[t][1]
-        z[i+1+j,dt] = data[i].SWC_2[t][1]
-        z[i+2+j,dt] = data[i].SWC_3[t][1]
-        z[i+3+j,dt] = data[i].SWC_4[t][1]
-        z[i+4+j,dt] = data[i].SWC_5[t][1]
-        z[i+5+j,dt] = data[i].SWC_6[t][1]
-    end
-end
-
 # Replace missing by NaN, for plotting
 x = replace(x, missing=>NaN)
 y = replace(y, missing=>NaN)
-z = replace(z, missing=>NaN)
-z = replace(z, 0.0=>NaN)
+SWC = replace(SWC, missing=>NaN)
+SWC = replace(SWC, 0.0=>NaN)
 # Initialize plot for gif
 using Plots
-scatter(x,y,color=:deep,markersize=10,zcolor=z[:,1])
+scatter(x,y,color=:deep,markersize=10,zcolor=SWC[175,:])
 xlabel!("x (m)")
 ylabel!("y (m)")
-title!(Dates.format(D1[1], "e, dd u yyyy HH:MM:SS"))
+title!(Dates.format(Dtime[175], "e, dd u yyyy HH:MM:SS"))
 plot!(xticks = 0:12.5:87.5)
 plot!(yticks = 0:12.5:87.5)
 plot!(legend = nothing)
 plot!(colorbar_title = "Soil Moisture")
 plot!(dpi = 300)
 # Make gif, fps = 2
-anim = @animate for i = 1:6
-    title!(Dates.format(D1[i], "e, dd u yyyy HH:MM:SS"))
-    scatter!(x,y,color=:deep,markersize=10,zcolor=z[:,i])
+anim = @animate for i = 175:m
+    title!(Dates.format(Dtime[i], "e, dd u yyyy HH:MM:SS"))
+    scatter!(x,y,color=:deep,markersize=10,zcolor=SWC[i,:])
 end
-gif(anim,"Output\\anim_fps2.gif",fps=2)
+gif(anim,"Output\\anim_5days.gif",fps=10)
 
 # 3D scatter with altitude
-# scatter3d(x,y,altitude,color=:deep,markersize=10,zcolor=z[:,1])
+# scatter3d(x,y,altitude,color=:deep,markersize=10,zcolor=SWC[:,1])
 # this connect the dots to create a surface
 # surface(x,y,altitude)
+
+# to do, no dot if missing data
+# to do, fix the colorbar range (from 0.35 to 0.5 for example)
